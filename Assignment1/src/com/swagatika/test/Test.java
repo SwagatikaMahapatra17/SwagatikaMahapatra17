9628d47420556654fc9495c10d844ddce4d9e253
dvl1 <dv1@gmail.com>
2021-07-25 20:58:25 +0530

    Created Test.java by TL

---
+++ Assignment1/src/com/swagatika/test/Test.java
@@ -0,0 +1,16 @@
+package com.swagatika.test;
+
+import com.swagatika.beans.Utility;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		Utility u = new Utility();
+		
+		System.out.println(u.getsystemDate());
+		
+
+	}
+
+}
